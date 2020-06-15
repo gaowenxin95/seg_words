3c538718669c242f997a264d282d4766c6fe3e70
gaowenxin95 <18133806830@163.com>
2020-06-15 17:10:27 +0800

    add #1

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='seg_words',
+      version='0.1',
+       description='textprocessing',
+       author='Wenxin Gao',
+       author_email='unique.gaowenxin@foxmail.com',
+       url='https://github.com/gaowenxin95/text_processing',
+       packages=['seg_words'],
+       install_requires = [],
+      )
+
